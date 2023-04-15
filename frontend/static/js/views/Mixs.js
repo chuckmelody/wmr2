e69f35fcc59df36dd3b7ad1b529e5e4f2029c449
chuckmelody <greg.henry@hotmail.co.uk>
2023-04-15 17:23:33 +0100

    update

--- frontend/static/js/views/Mixs.js
+++ frontend/static/js/views/Mixs.js
@@ -1,5 +1,6 @@
 import AbstractView from "./AbstractView.js";
 import wmrTabListCon from "./Module/TabsListCon.js";
+import MixView from "./MixView.js";
 
 export default class extends AbstractView {
   constructor(params) {
@@ -7,37 +8,49 @@ export default class extends AbstractView {
     this.setTitle("Mix");
   }
 
+  async render() {
+    const html = await this.getHtml();
+    this.container.innerHTML = html;
+    await this.afterRender();
+    return;
+  }
+
   async getHtml() {
     return `
-      <section>
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-sm-12 col-md-4">
-              <div class="playlist">
-                <div class="py-5" id="playlist-modal">
-                
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-12 col-md-4">
-              <div class="row">
-                <div class="py-5" id="contentb">
-                  house
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-12 col-md-4">
-              Right bar
+    <section>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-12 col-md-4">
+          <div class="playlist">
+            <div class="py-5" id="playlist-modal">
+            
             </div>
           </div>
         </div>
-      </section>
+        <div class="col-sm-12 col-md-4">
+          <div class="row">
+            <div class="py-5" id="contentb">
+              house
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4">
+          Right bar
+        </div>
+      </div>
+    </div>
+  </section>
     `;
   }
+
   async afterRender() {
-    // Add any additional content or event listeners here
-    const thrh = document.querySelector("#playlist-modal");
-    thrh.innerHTML = "";
-    thrh.innerHTML = wmrTabListCon();
+    const playlistModal = document.querySelector("#playlist-modal");
+    playlistModal.innerHTML = "";
+    playlistModal.appendChild(document.createElement("h1")).textContent =
+      "In Gods Name";
+
+    console.log(playlistModal);
   }
 }
+const mixView = new MixView();
+mixView.render();

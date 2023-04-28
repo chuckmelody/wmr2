ae09f530d8b85b80904b94c7c4da672597da410a
chuckmelody <greg.henry@hotmail.co.uk>
2023-04-28 20:49:41 +0100

    Update

--- frontend/static/js/views/Mixs.js
+++ frontend/static/js/views/Mixs.js
@@ -9,164 +9,195 @@ export default class extends AbstractView {
 
   async getHtml() {
     return `
-      <section>
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-sm-12 col-md-4">
-              <div class="playlist">
+    <section>
+    <div class="container-fluid">
+       <div class="row">
+          <div class="col-sm-12 col-md-4">
+             <div class="playlist">
                 <div class="py-1 playlist-modal" id="playlist-modal">
-                ${wmrTabListCon()}
+                   ${wmrTabListCon()}
                 </div>
-              </div>
-            </div>
-            <div class="col-sm-12 col-md-4">
-              <div class="mt-4 p-5" id="wmrNowPlayDp">
-              <div class="wmrNowPlayDpInner mt-4 pt-1">
-                <div class="row">
-                Switch
-                </div>
-                <div class="row">
-                Tos mid bass Btn
-                </div>
-                <div class="row">
-                Solo
-                </div>
-                
-                <div class="row">
-                  <div class="wmr-mix-channalCon d-flex justify-content-center w-100 gap-2 px-0 text-center">
-                    
-                  <div class="wmrVolDarkBG" id="wmrMeterConCh1">
-                  <span>0</span>
-                    <div class="my-0">
-                    <canvas class="m-0" id="wmCh1" width="25" height="205" data-val="0">No canvas</canvas>
-                    
-                    </div>
-                  </div>
-                  <div class="wmrVolDarkBG">
-                    <span>Level <span>0</span></span>
-                      <div class="wmr-channel my-3">
-                        <div class="wmrVolLeftCon wmr-channel-inner">
-                        <input class="slider" id="wmrVolLeftRange" type="range" min="0" step="1" max="10" value="0">
-                        </div>
-                      </div>
-                      <span>CH 1</span>
-                    </div>
+             </div>
+          </div>
+          <div class="col-sm-12 col-md-4">
+             <div class="mt-4 p-4 pt-2" id="wmrNowPlayDp">
+             <div class="row pt-2">
+             <div class="d-flex justify-content-between align-items-center px-4">
+             <div id="wmr-logo" class="fw-bold position-relative">
+                                    <sup id="wmr-logo-td">™</sup>
+                                    <h1 class="text-wmr2">Baracoda
+                                        
+                                        
+                                    </h1>
 
-                    <div class="wmrVolDarkBG" id="wmrMeterConCh2">
-                    <span>0</span>
-                      <div class="my-0">
-                      <canvas class="m-0" id="wmCh2" width="25" height="205" data-val="0">No canvas</canvas>
-                      
-                      </div>
-                    </div>
-
-
-
-
-                    <div class="wmrVolDarkBG">
-                    <span>Level <span>0</span></span>
-                      <div class="wmr-channel my-3">
-                        <div class="wmr-channel-inner">
-                        
-                        </div>
-                      </div>
-                      <span>CH 2</span>
-                    </div>
-                    <div class="wmrVolDarkBG" id="wmrMeterConCh2">
-                    <span>0</span>
-                      <div class="my-0">
-                      <canvas class="m-0" id="wmCh2" width="25" height="205" data-val="0">No canvas</canvas>
-                      
-                      </div>
-                    </div>
-                    <div class="wmrVolDarkBG">
-                    <span>Level <span>0</span></span>
-                      <div class="wmr-channel my-3">
-                        <div class="wmrVolRightCon wmr-channel-inner3">
-                        <input class="slider" id="wmrVolRightRange" type="range" min="0" step="1" max="10" value="0">
+                                </div>
+             <div>
+             <span class="wmrM700">Media Controller</span> <span class="text-wmr"><h5 class="text-wmr2">DJM-700</h5></span>
+             </div>
+             </div>
+             </div>
+                <div class="wmrNowPlayDpInner my-0 p-2 pb-3 mb-4">
+                   <div class="row mt-3">
+                      <div class="wmrMixEffectCon d-flex justify-content-center gap-2 px-4">
+                         <div class="col-sm-6">
+                            <div class="card border-0 wmrMixEffectCd">
+                               <div class="card-head py-1">
+                                  Aplitude Range
+                               </div>
+                               <div class="card-body p-1">
+                                  <div class="d-flex justify-content-between align-content-center px-2">
+                                     <div>
+                                        <img class="img-fluid" id="wmrMixerTops" src="./static/img/mixdesk/Knob_mid_064.png" alt="Tops Knob">
+                                        <div class="pb-2">HIGH</div>
+                                        <div class="d-flex justify-content-between align-items-center gap-2">
+                                        <button class="btn btn-dark min-btn">-</button> 
+                                        <button class="btn btn-dark plus-btn">+</button>
+                                        </div>
+                                     </div>
+                                     <div>
+                                        <img class="img-fluid" id="wmrMixerMid" src="./static/img/mixdesk/Knob_mid_064.png" alt="Mid Range Knob">
+                                        <div class="pb-2">MID</div>
+                                        <div class="d-flex justify-content-between align-items-center gap-2">
+                                        <button class="btn btn-dark min-btn">-</button> 
+                                        <button class="btn btn-dark plus-btn">+</button>
+                                        </div>
+                                     </div>
+                                     <div>
+                                        <img class="img-fluid" id="wmrMixerBass" src="./static/img/mixdesk/Knob_mid_064.png" alt="Bass Knob">
+                                        <div class="pb-2">LOW</div>
+                                        <div class="d-flex justify-content-between align-items-center gap-2">
+                                        <button class="btn btn-dark min-btn">-</button> 
+                                        <button class="btn btn-dark plus-btn">+</button>
+                                        </div>
+                                     </div>
+                                  </div>
+                               </div>
+                            </div>
+                         </div>
+                         <div class="col-sm-2">
+                         <div class="card border-0 wmrMixEffectCd">
+                          <div class="card-head py-1">
+                                Cut
                           </div>
+                          <div class="card-body p-1">
+                          <div class="wmrcut d-flex flex-column gap-2 justify-content-between align-content-center py-2 ">
+                          <button type="button" class="btn btn-dark plus-btn btn-block btn-small text-secondary">High</button>
+                          <button type="button" class="btn btn-dark plus-btn btn-block btn-small text-secondary">Mid</button>
+                          <button type="button" class="btn btn-dark plus-btn btn-block btn-small text-secondary">Low</button>
+                          </div> 
+                              </div>
+                         </div>
+                         </div>
+                         <div class="col-sm-4">
+                            <div class="card border-0 wmrMixEffectCd">
+                               <div class="card-head py-1">
+                                  Main
+                               </div>
+                               <div class="card-body p-1">
+                                  <div class="d-flex justify-content-between gap-2 align-content-center px-2">
+                                     
+                                     <div>
+                                        <img class="img-fluid" id="wmrMixerMid" src="./static/img/mixdesk/Knob_mid_064.png" alt="Mid Range Knob">
+                                        <div class="pb-2">PITCH</div>
+                                        <div class="d-flex justify-content-between align-items-center gap-2">
+                                        <button class="btn btn-dark min-btn">-</button> 
+                                        <button class="btn btn-dark plus-btn">+</button>
+                                        </div>
+                                     </div>
+                                     <div>
+                                        <img class="img-fluid" id="wmrMixerBass" src="./static/img/mixdesk/Knob_mid_064.png" alt="Bass Knob">
+                                        <div class="pb-2">MASTER</div>
+                                        <div class="d-flex justify-content-between align-items-center gap-2">
+                                        <button class="btn btn-dark min-btn">-</button> 
+                                        <button class="btn btn-dark plus-btn">+</button>
+                                        </div>
+                                     </div>
+                                  </div>
+                               </div>
+                            </div>
+                            
+                         </div>
                       </div>
-                      <span>CH 3</span>
-                    </div>
-
-                  </div>
+                      <div class="row m-0 my-2">
+                      <div class="d-flex justify-content-between wmrMixEffectCd gap-2 py-2 mx-1" style="max-width: 98.5%">
+                      <button class="btn btn-dark plus-btn text-secondary">Playlist</button>
+                      <button class="btn btn-dark plus-btn text-secondary">Details</button>
+                      <button class="btn btn-dark plus-btn text-secondary">Favorites</button>
+                      <button class="btn btn-dark plus-btn text-secondary">History</button>
+                      
+                      <button class="btn btn-dark plus-btn text-secondary">Effects</button>
+                      <button class="btn btn-dark plus-btn text-secondary">Player</button>
+                      </div>
+                      </div>
+                      <div class="row w-100 m-0">
+                         <div class="wmr-mix-channalCon d-flex justify-content-center gap-2 text-center px-1">
+                            <div class="wmrVolDarkBG" id="wmrMeterConCh1">
+                               <span class="bg-black p-1 px-2 rounded-1" >0</span>
+                               <div class="my-0">
+                                  <canvas class="m-0" id="wmCh1" width="25" height="205" data-val="0">No canvas</canvas>
+                               </div>
+                            </div>
+                            <div class="wmrVolDarkBG">
+                               <button class="btn btn-dark wmrMute" id="wmrCh1Mute">Mute</button>
+                               <div class="wmr-channel my-3">
+                                  <div class="wmrVolLeftCon wmr-channel-inner">
+                                     <input class="slider" id="wmrVolLeftRange" type="range" min="0" step="1" max="10" value="0">
+                                  </div>
+                               </div>
+                               <span>MIC</span>
+                            </div>
+                            <div class="wmrVolDarkBG" id="wmrMeterConCh2">
+                               <span class="bg-black p-1 px-2 rounded-1">0</span>
+                               <div class="my-0">
+                                  <canvas class="m-0" id="wmCh2" width="25" height="205" data-val="0">No canvas</canvas>
+                               </div>
+                            </div>
+                            <div class="wmrVolDarkBG">
+                               <button class="btn btn-dark wmrMute" id="wmrCh2Mute">Mute</button>
+                               <div class="wmr-channel my-3">
+                                  <div class="wmrVolMiddleCon wmr-channel-inner">
+                                     <input class="slider" id="wmrVolMiddleRange" type="range" min="0" step="1" max="10" value="0">
+                                  </div>
+                               </div>
+                               <span>JINGLES</span>
+                            </div>
+                            <div class="wmrVolDarkBG" id="wmrMeterConCh3">
+                               <span class="bg-black p-1 px-2 rounded-1">0</span>
+                               <div class="my-0">
+                                  <canvas class="m-0" id="wmCh3" width="25" height="205" data-val="0">No canvas</canvas>
+                               </div>
+                            </div>
+                            <div class="wmrVolDarkBG">
+                               <button class="btn btn-dark wmrMute" id="wmrCh3Mute">Mute</button>
+                               <div class="wmr-channel my-3">
+                                  <div class="wmrVolRightCon wmr-channel-inner3">
+                                     <input class="slider" id="wmrVolRightRange" type="range" min="0" step="1" max="10" value="0">
+                                  </div>
+                               </div>
+                               <span>MIXCLOUD</span>
+                            </div>
+                         </div>
+                      </div>
+                      <div class="row">
+                         
+                      </div>
+                   </div>
                 </div>
-                
-                <div class="row">
-                Cross Fade
-                </div>
-                </div>
-              </div>
-
+             </div>
+          </div>
+          <div class="col-12 col-md-4">
+          <div class="px-4 pt-5 d-flex justify-content-center align-items-center position-relative" id="wmrNowPlayCon">
+            <div id="wmrNowPlayConInner">
+            <img src="./static/img/wmr/covers/des.jpg" alt="Mix Image ">
             </div>
-          
-            <div class="col-12 col-md-4">
-              <div class="px-4 pt-5 d-flex justify-content-center align-items-center position-relative" id="wmrNowPlayCon">
-                <div id="wmrNowPlayConInner">
-                <img src="./static/img/wmr/covers/des.jpg">
-                  
-                </div>
-                <div id="wmrNowPlayConOuter" class="w-100">
-                    <img img-fluid src="./static/img/wmr/covers/TabScreenFrame400.png">
-                  </div>
-              </div>
+            <div id="wmrNowPlayConOuter" class="w-100">
+                <img class="img-fluid" src="./static/img/wmr/covers/TabScreenFrame400.png" alt="Tab Screen">
             </div>
-
           </div>
         </div>
-      </section>
+       </div>
+    </div>
+ </section>
     `;
   }
 }
-
-{
-  /* <div class="d-flex justify-content-between text-center mb-1">
-            
-            
-</div>
-
-
-
-<div class="row d-flex justify-content-between text-center">
-<div class="col-sm-12 col-md-3 wmrVolDarkBG py-4">
-
-  <div class="wmrFadScaleLeft">
-  <div class="wmrVolLeftCon">
-       <input class="slider" id="wmrVolLeftRange" type="range" min="0" step="1" max="10" value="0">
-    </div>
-  
-  </div>
-</div>
-<div class="col-sm-12 col-md-4">
-  <div>
-  Center
-  </div>
-</div>
-<div class="col-sm-12 col-md-4 wmrVolDarkBG py-4">
-
-  <div Class="wmrFadScaleRight">
-  <div class="wmrVolRightCon">
-       <input class="slider" id="wmrVolRightRange" type="range" min="0" max="10" value="0">
-    </div>
-  </div>
-</div>
-</div> */
-}
-
-// async function getmixCategory() {
-//   await fetch(`http://localhost:3000/songs`)
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("Network Error");
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//       console.log(data);
-//     })
-//     .catch((error) => {
-//       console.error("The has been a problem", error);
-//     });
-// }
-
-// getmixCategory();
